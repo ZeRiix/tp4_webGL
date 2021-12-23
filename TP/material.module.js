5ab5850acc60660ce2eeba6d8d2a237b893ff2ad
Vitaalx <liamdu92@gmail.com>
2021-12-23 19:29:52 +0100

    Added new textures, option to change floorTexture and fixed bugs

---
+++ TP/material.module.js
@@ -0,0 +1,21 @@
+import * as THREE from '../build/three.module.js';
+
+const groundMarstexture = new THREE.TextureLoader().load('./textures/sol-texture-mars.jpg');
+const groundMoontexture = new THREE.TextureLoader().load('./textures/texture-moon.jpg');
+const groundNeptunetexture = new THREE.TextureLoader().load('./textures/texture-neptune.jpg');
+const groundSuntexture = new THREE.TextureLoader().load('./textures/texture-sun.jpg');
+
+const groundMoonmaterial = new THREE.MeshPhongMaterial({ map: groundMoontexture });
+const groundNeptunematerial = new THREE.MeshPhongMaterial({ map: groundNeptunetexture });
+const groundSunmaterial = new THREE.MeshPhongMaterial({ map: groundSuntexture });
+const groundMarsmaterial = new THREE.MeshPhongMaterial({ map: groundMarstexture });
+
+const groundFileList = {
+    "Mars": groundMarsmaterial,
+    "Lune": groundMoonmaterial,
+    "Soleil": groundSunmaterial,
+    "Neptune": groundNeptunematerial
+    
+    };
+
+export { groundFileList }
